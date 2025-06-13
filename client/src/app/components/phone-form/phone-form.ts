cba8d0f9b1fc5f0a87567476b3ef7bdc06c088bc
Jakub Jezior <jakubjezior@gmail.com>
2025-06-13 10:07:29 +0200

    Added phone form component

--- client/src/app/components/phone-form/phone-form.ts
+++ client/src/app/components/phone-form/phone-form.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PhoneModel } from '../../models/phone-model';
 
 @Component({
   selector: 'app-phone-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './phone-form.html',
   styleUrl: './phone-form.scss'
 })
 export class PhoneForm {
+  @Input() phone: PhoneModel = {  number: '', name: '' };
+  @Output() save = new EventEmitter<PhoneModel>();
+  @Output() cancel = new EventEmitter<void>();
 
+  onSubmit() {
+    this.save.emit(this.phone);
+  }
 }
