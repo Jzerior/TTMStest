4ce80f60c20bf93f166f40b11cb242e66b498ba6
Jakub Jezior <jakubjezior@gmail.com>
2025-06-13 09:15:09 +0200

    Added Phone repository interface

---
+++ server/Interfaces/IRepositories/IPhoneRepository.cs
@@ -0,0 +1,16 @@
+using TTMStest.Server.Models;
+using System.Collections.Generic;
+
+    
+namespace TTMStest.Server.Interfaces.IRepositories
+{
+
+    public interface IPhoneRepository
+    {
+        Task<IEnumerable<Phone>> GetAllPhonesAsync();
+        Task<Phone?> GetPhoneByIdAsync(int id);
+        Task<Phone> AddPhoneAsync(Phone phone);
+        Task<Phone?> UpdatePhoneAsync(Phone phone);
+        Task<Phone?> DeletePhoneAsync(int id);
+    }
+}
