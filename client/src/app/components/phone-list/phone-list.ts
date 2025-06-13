bce754b4590938ea253063e917eb8fcae1c7f71c
Jakub Jezior <jakubjezior@gmail.com>
2025-06-13 10:13:11 +0200

    Added phone list component

--- client/src/app/components/phone-list/phone-list.ts
+++ client/src/app/components/phone-list/phone-list.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { PhoneModel } from '../../models/phone-model';
 
 @Component({
   selector: 'app-phone-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './phone-list.html',
   styleUrl: './phone-list.scss'
 })
 export class PhoneList {
-
+  @Input() phones: PhoneModel[] = [];
+  @Output() edit = new EventEmitter<PhoneModel>();
+  @Output() delete = new EventEmitter<number>();
 }
